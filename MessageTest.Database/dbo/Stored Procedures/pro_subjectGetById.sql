23d1f60fa41a72fb0af0bbe69f1be5e41594579b
Gavin <j5785481@gmail.com>
2026-05-18 14:53:59 +0800

    [Fixed]修改getById SP

--- MessageTest.Database/dbo/Stored Procedures/pro_subjectGetById.sql
+++ MessageTest.Database/dbo/Stored Procedures/pro_subjectGetById.sql
@@ -9,11 +9,8 @@
 CREATE PROCEDURE [dbo].[pro_subjectGetById]
     @f_Id INT = NULL  -- 設定預設值為 NULL
 AS 
-BEGIN
-    SET NOCOUNT ON;
-
-    SELECT 
-        f_id, f_title, f_content, f_creatorId, f_createdAt, f_messageCount
-    FROM t_subject WITH (NOLOCK)
-    WHERE (@f_Id IS NULL OR f_id = @f_Id)
-END
+SELECT 
+	f_id, f_title, f_content, f_creatorId, f_createdAt, f_messageCount
+FROM t_subject WITH (NOLOCK)
+WHERE (@f_Id IS NULL OR f_id = @f_Id)
+RETURN 0
