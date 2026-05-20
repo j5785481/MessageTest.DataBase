52045a929f3d0e32cac153b68f3bd507b17f8a2d
Gavin <j5785481@gmail.com>
2026-05-20 11:20:08 +0800

    [Added]新增Upsert SP將messageCount Update整合進Upsert [Deleted]刪除messageCount增加、減少SP

---
+++ MessageTest.Database/dbo/Stored Procedures/pro_subjectUpsert.sql
@@ -0,0 +1,35 @@
+/*
+	描述: Upsert主題
+
+	exec pro_subjectUpsert
+		@title = '我是主旨'
+		@content = '我是內容'
+		@creatorId = '114060601'
+*/
+
+CREATE PROCEDURE [dbo].[pro_subjectUpsert]
+	@f_id			INT,
+	@f_title		NVARCHAR(20),
+	@f_content		NVARCHAR(MAX),
+	@f_creatorId	NVARCHAR(20),
+	@f_creatorAt	DATETIME2(7),
+	@f_messageCount INT
+AS
+IF EXISTS (SELECT 1 FROM t_subject WITH (NOLOCK) WHERE f_id = @f_id)
+	BEGIN
+		UPDATE t_subject WITH (ROWLOCK) SET
+			f_id = @f_id,
+			f_title = @f_title,
+			f_content = @f_content,
+			f_creatorId = @f_creatorId,
+			f_messageCount = @f_messageCount
+		OUTPUT inserted.*
+		WHERE f_id = @f_id
+	END
+	ELSE
+	BEGIN
+		INSERT INTO t_subject (f_title, f_content, f_creatorId)
+		OUTPUT inserted.*
+		VALUES (@f_title, @f_content, @f_creatorId)
+	END
+RETURN 0
